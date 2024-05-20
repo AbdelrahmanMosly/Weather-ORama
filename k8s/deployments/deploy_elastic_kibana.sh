9c3b8e8ca6ad3197eba69293070b7cf0b5312ceb
Karim Alaa <karimalaa1912@gmail.com>
2024-05-20 10:41:46 +0300

    k8s are working

---
+++ k8s/deployments/deploy_elastic_kibana.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl apply -f elastic-kibana-deployment.yaml
+kubectl apply -f elastic-uploader-deployment.yaml
---
+++ k8s/deployments/deploy_kafka.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl apply -f zookeeper-deployment.yaml
+kubectl apply -f kafka-deployment.yaml
+                
